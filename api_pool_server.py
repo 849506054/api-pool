1c1e27c2c3bb536affeca1075627fd58b3a8fd75
849506054 <849506054@users.noreply.github.com>
2026-06-27 11:28:52 +0800

    chore: default health_mode to models (zero-cost) instead of chat

--- api_pool_server.py
+++ api_pool_server.py
@@ -368,7 +368,7 @@ class Endpoint:
     
     _today_used: int = field(default=0, repr=False)
     _today_date: str = field(default="", repr=False)
-    health_mode: str = field(default="chat")
+    health_mode: str = field(default="models")
 
     _health: str = field(default="unknown", repr=False) 
     _health_latency_ms: int = field(default=-1, repr=False)
